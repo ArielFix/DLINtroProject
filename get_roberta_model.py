03ce33e53f37c2f3be964bc31d63437a4170adc1
ArielFix <ariel.f1990@gmail.com>
2023-02-15 00:05:33 +0200

    Final

--- get_roberta_model.py
+++
@@ -1,6 +0,0 @@
-from pre_trained_roberta.get_pretrained_roberta import GetPretrainedRoberta
-
-print('start')
-model = GetPretrainedRoberta().get_roberta()
-print('moduled: ', model.modules)
-model.modules
